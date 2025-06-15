811fd4f6ffb3023e8ea97b3233920b4b54f22dca
Natalie Rekai <ncrekai@gmail.com>
2025-06-15 12:25:21 -0400

    up to accessibility

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,8 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
- 
-export default function Layout({ children }: { children: React.ReactNode }) {
+
+export const experimental_ppr = true;
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
@@ -10,3 +12,4 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     </div>
   );
 }
+export default Layout;
